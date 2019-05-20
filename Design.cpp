a152e903c19ea2a578ad14c77c0dd1881b40d098
Pdemers026 <49468174+Pdemers026@users.noreply.github.com>
2019-05-20 15:19:07 -0700

    Update Design.cpp

--- Design.cpp
+++ Design.cpp
@@ -1,184 +1,165 @@
-#include <string>
-#include <iostream>
-#include<fstream>
-#include<vector>
-#include<time.h>
-using namespace std;
-
-//Design Declaration
 class Design
 {
 public:
 
-	int TriesLeft(string word, string guessed);
-	string LoadRandomWord(string path);
-	void PrintAvailableLetters(string taken);
-	void PrintMessage(string, bool, bool);
-	void DrawHangman(int g);
-	void PrintLetters(string input, char from, char to);
-	bool PrintWordAndCheckWin(string word, string guessed);
+int TriesLeft(string word, string guessed);
+string LoadRandomWord(string path);
+void PrintAvailableLetters(string taken);
+void PrintMessage(string, bool, bool);
+void DrawHangman(int g);
+void PrintLetters(string input, char from, char to);
+bool PrintWordAndCheckWin(string word, string guessed);
 
 };
 
+
 //Design Class definitions
-void Design::PrintMessage(string message, bool printTop = true, bool printBottom = true)
+void Design::PrintMessage(string message, bool printTop = true, bool printBottom = true)//Important function that centers all of the strings and makes it look nice
 {
-	if (printTop)
-	{
-		cout << "+---------------------------------+" << endl;
-		cout << "|";
-	}
-	else
-	{
-		cout << "|";
-	}
-	bool front = true;
-	for (int i = message.length(); i < 33; i++)
-	{
-		if (front)
-		{
-			message = " " + message;
-		}
-		else
-		{
-			message = message + " ";
-		}
-		front = !front;
-	}
-	cout << message.c_str();
-
-	if (printBottom)
-	{
-		cout << "|" << endl;
-		cout << "+---------------------------------+" << endl;
-	}
-	else
-	{
-		cout << "|" << endl;
-	}
+if (printTop)
+{
+cout << "+---------------------------------+" << endl;
+cout << "|";
 }
-void Design::DrawHangman(int guessCount = 0)
+else
 {
-	if (guessCount >= 1)
-		PrintMessage("|", false, false);
-	else
-		PrintMessage("", false, false);
-
-	if (guessCount >= 2)
-		PrintMessage("|", false, false);
-	else
-		PrintMessage("", false, false);
-
-	if (guessCount >= 3)
-		PrintMessage("O", false, false);
-	else
-		PrintMessage("", false, false);
-
-	if (guessCount == 4)
-		PrintMessage("/  ", false, false);
-
-	if (guessCount == 5)
-		PrintMessage("/| ", false, false);
-
-	if (guessCount >= 6)
-		PrintMessage("/|\\", false, false);
-	else
-		PrintMessage("", false, false);
-
-	if (guessCount >= 7)
-		PrintMessage("|", false, false);
-	else
-		PrintMessage("", false, false);
-
-	if (guessCount == 8)
-		PrintMessage("/  ", false, false);
-
-	if (guessCount >= 9)
-		PrintMessage("/ \\", false, false);
-	else
-		PrintMessage("", false, false);
+cout << "|";
 }
-void Design::PrintLetters(string input, char from, char to)
+bool front = true;
+for (int i = message.length(); i < 33; i++)
 {
-	//takes input string and makes all char uppercase
-	for (int i = 0; i < input.length(); i++)
-	{
-		input[i] = toupper(input[i]);
-	}
-	//prints string
-	string s;
-	for (char i = from; i <= to; i++)
-	{
-		if (input.find(i) == string::npos)
-		{
-			s += i;
-			s += " ";
-		}
-		else
-			s += "  ";
-	}
-	PrintMessage(s, false, false);
+if (front)
+{
+message = " " + message;
 }
-void Design::PrintAvailableLetters(string taken)
+else
 {
-	PrintMessage("Available letters");
-	PrintLetters(taken, 'A', 'M');
-	PrintLetters(taken, 'N', 'Z');
+message = message + " ";
 }
-bool Design::PrintWordAndCheckWin(string word, string guessed)
+front = !front;
+}
+cout << message;
+
+if (printBottom)
 {
-	bool won = true;
-	string s;
-	for (int i = 0; i < word.length(); i++)
-	{
-		if (guessed.find(word[i]) == string::npos)
-		{
-			won = false;
-			s += "_ ";
-		}
-		else
-		{
-			s += word[i];
-			s += " ";
-		}
-	}
-	PrintMessage(s, false);
-	return won;
+cout << "|" << endl;
+cout << "+---------------------------------+" << endl;
+}
+else
+{
+cout << "|" << endl;
+}
+}
+void Design::DrawHangman(int guessCount = 0)// This will build the hangman as the player guesses
+{
+if (guessCount >= 1)
+PrintMessage("|", false, false);
+else
+PrintMessage("", false, false);
+
+if (guessCount >= 2)
+PrintMessage("|", false, false);
+else
+PrintMessage("", false, false);
+
+if (guessCount >= 3)
+PrintMessage("O", false, false);
+else
+PrintMessage("", false, false);
+
+if (guessCount == 4)
+PrintMessage("|", false, false);
+
+if (guessCount == 5)
+PrintMessage("/| ", false, false);
+
+if (guessCount >= 6)
+PrintMessage("/|\\", false, false);
+else
+PrintMessage("", false, false);
+
+if (guessCount >= 7)
+PrintMessage("|", false, false);
+else
+PrintMessage("", false, false);
+
+if (guessCount == 8)
+PrintMessage("/  ", false, false);
+
+if (guessCount >= 9)
+PrintMessage("/ \\", false, false);
+else
+PrintMessage("", false, false);
+}
+void Design::PrintLetters(string input, char from, char to) //Will check to see if guessed letter(s) from A-M  or N-Z
+{
+string s;
+for (char i = from; i <= to; i++)
+{
+if (input.find(i) == string::npos)//if guessed letter is not found then it will remain on the board 
+{
+s += i;
+s += " "; 
+}
+else //else if the letter is found it will be taken off the board.
+s += "  ";
+}
+PrintMessage(s, false, false);
+}
+void Design::PrintAvailableLetters(string taken)//This will print out the available letters
+{
+PrintMessage("Available Letters");
+PrintLetters(taken, 'A', 'M');
+PrintLetters(taken, 'N', 'Z');
+}
+bool Design::PrintWordAndCheckWin(string word, string guessed)// Checking the guesses for a match with the word
+{
+bool won = true;
+string s;
+for (int i = 0; i < word.length(); i++)
+{
+if (guessed.find(word[i]) == string::npos)//compares guess to random word, if guess doesnt match it will just show the place holders along with any guessed letter.
+{
+won = false;
+s += "_ ";
+}
+else//else when guess correctly the place holder will be replaced with the letter
+{
+s += word[i];
+s += " ";
+}
+}
+PrintMessage(s, false);
+return won;
 }
 string Design::LoadRandomWord(string path)
 {
-	int lineCount = 0;
-	string word;
-	vector<string> v;
+int lineCount = 0;
+string word;
+vector<string> v;
 
-	//get random word from external file
-	ifstream reader(path);
-	if (reader.is_open())
-	{
-		while (std::getline(reader, word))
-			v.push_back(word);
-
-		int randomLine = rand() % v.size();
-
-		word = v.at(randomLine);
-		reader.close();
-	}
-	else
-	{
-		cout << "There was an error opening the file.\n"
-			<< "Please check the name sent to LoadRandomWord and try again\n";
-		return "-1";
-	}
-
-	return word;
-}
-int Design::TriesLeft(string word, string guessed)
+//get random word from external file
+ifstream reader(path);
+if (reader.is_open())
 {
-	int error = 0;
-	for (int i = 0; i < guessed.length(); i++)
-	{
-		if (word.find(guessed[i]) == string::npos)
-			error++;
-	}
-	return error;
+while (getline(reader, word))//This will create a vector of strings 
+v.push_back(word);
+
+int randomLine = rand() % v.size();//generates a random number between 0 and the  max size of the vector
+
+word = v[randomLine];//word to guess is selected
+reader.close();
+}
+
+return word;
+}
+int Design::TriesLeft(string word, string guessed)//This will determine how many letters they get wrong
+{
+int error = 0;
+for (int i = 0; i < guessed.length(); i++)
+{
+if (word.find(guessed[i]) == string::npos)//if word doesn't find a match with the guess the error count will increment by one
+error++;
+}
+return error;
 }
